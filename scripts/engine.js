1d5f1ae6e3ab5419f6694abfbb914fe50a480730
vyara <vyara_hristova@yahoo.de>
2015-07-25 14:21:00 +0300

    created a kinetic stage where the layers can be added to

--- scripts/engine.js
+++ scripts/engine.js
@@ -1,3 +1,13 @@
-window.onload = function(){
-    //put kinetic layers here
+window.onload = function () {
+    var CONSTANTS = {
+            STAGE_WIDTH: 800,
+            STAGE_HEIGHT: 500
+        },
+
+        stage = new Kinetic.Stage({
+            container: 'container',
+            width: CONSTANTS.STAGE_WIDTH,
+            height: CONSTANTS.STAGE_HEIGHT
+        });
+    //put kinetic layers here e.g stage.add(layerName);
 }
