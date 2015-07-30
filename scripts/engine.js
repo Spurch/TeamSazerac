99e4457832c13dbfb49104938d151ac51dcdda7b
Chavdar Angelov <c.l.angelov@gmail.com>
2015-07-30 21:13:32 +0300

    Revert "updated logic how to generate numbers"
    This reverts commit 3010a16c88df25b96ba99058772e1ca1c7139f35.

--- scripts/engine.js
+++ scripts/engine.js
@@ -82,11 +82,11 @@ window.onload = function () {
         function timerCount() {
             times = minutes + ':' + seconds;
             if (seconds < 59) {
-                // console.log(times);
+                console.log(times);
             } else {
                 seconds = -1;
                 minutes++;
-                // console.log(times);
+                console.log(times);
 
             }
 
@@ -265,8 +265,7 @@ window.onload = function () {
 
     function setSoftDrinkImage(softDrinkObject) {
         var softDrinkImage = new Image();
-        var number = Math.ceil(Math.random() * softDrinkSources.length)-1;
-        // console.log('soft index ' + number);
+        var number = Math.round(Math.random() * softDrinkSources.length-1);
         softDrinkImage.src = softDrinkSources[number];
 
         softDrinkImage.onload = function () {
@@ -281,8 +280,7 @@ window.onload = function () {
 
     function setCocktailImage(cocktailObject) {
         var cocktailImage = new Image();
-        var number = Math.ceil(Math.random() * cocktailSources.length)-1;
-        // console.log('cockt index ' + number);
+        var number = Math.round(Math.random() * cocktailSources.length-1);
         cocktailImage.src = cocktailSources[number];
 
         cocktailImage.onload = function () {
@@ -540,9 +538,8 @@ window.onload = function () {
 
     }, false);
 
-    /* test onblur and onfocus
+    // test onblur and onfocus
     setInterval(function () { 
-      	console.log(isActive ? 'active' : 'inactive'); 
+      console.log(isActive ? 'active' : 'inactive'); 
     }, 1000);
-	*/
 };
